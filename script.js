7650dab78cdbebd45e80cf0e358bae3a76d4a7e0
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-11-19 20:49:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let map = {};
+
+  for(let char of str)
+	  {
+		  map[char] = (map[char] || 0) + 1;
+	  }
+  for(let char of str)
+	  {
+		  if(map[char] == 1) return char;
+	  }
+
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
